4036f8e3b415c96c0e13d16d6001ea65688ec479
ChengMioa <2123816227@qq.com>
2025-10-14 00:03:34 +0800

    课后习题

---
+++ 第一章/t1.4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+	std::cout << "两数相乘" << std::endl;
+	int a = 0, b = 0;
+	std::cout << "输入第一个数:" << std::endl;
+	std::cin >> a;
+	std::cout << "输入第二个数:" << std::endl;
+	std::cin >> b;
+	std::cout << "两数相乘的结果是:" << a * b;
+	return 0;
+}
---
+++ 第一章/t1.8.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+	std::cout << "/*";  //正确
+	std::cout << "*/";  //正确
+	//std::cout << /*"*/"*/;  //错误
+	std::cout << /*"*/" /* "/*" */;//正确
+	return 0;
+}
